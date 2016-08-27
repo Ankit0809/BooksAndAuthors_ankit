fa2089a84184c54f878427823618beb8e6e1e0ca
ControlFreak <ControlFreak>
2016-08-28 01:32:45 +0530

    Initial commit

---
+++ booksAndAuthors.js
@@ -0,0 +1,16 @@
+
+module.exports = function (server, db) {
+
+    server.get('/api/books/:id', function (req, res, next) {
+        console.log(req.params.id);
+        db.BooksAndAuthor.find({$or:[{Book_id:req.params.id},{_id:req.params.id}]}, function (err, data) {
+            console.log(data);
+                res.writeHead(200, {
+                    'Content-Type': 'application/json; charset=utf-8'
+                });
+                res.end(JSON.stringify(data));
+            });
+        return next();
+    });
+
+}
